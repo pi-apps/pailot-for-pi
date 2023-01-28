40275673e96518546ff92679eb8d77e3d0a79285
tjfaithpro <tjfaithpro@gmail.com>
2023-01-28 08:31:19 +0100

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Home, SplashScreen, WelcomeScreen } from './pages';
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { motion } from "framer-motion"
+
 function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<SplashScreen />} />
-				<Route path='/welcome' element={<WelcomeScreen />} />
-				<Route path='/home' element={<Home />} />
-			</Routes>
+			<motion.div
+				initial={{ x: '100%', opacity: 0 }}
+				animate={{ x: 0, opacity: 1 }}
+				exit={{ x: '-100%', opacity: 0 }}
+			>
+				<Routes>
+					<Route path='/' element={<SplashScreen />} />
+					<Route path='/welcome' element={<WelcomeScreen />} />
+					<Route path='/home' element={<Home />} />
+				</Routes>
+			</motion.div>
 		</BrowserRouter>
 	)
 }
