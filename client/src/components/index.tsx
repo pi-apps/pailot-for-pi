f9c6788df80135018a61782864f9ad500534d484
Tejiri Faith <55920490+tjfaithpro@users.noreply.github.com>
2023-01-28 14:03:24 +0100

    Feat/2 create a splash screen (#17)
    * work on splash screen and welcome
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Work on splash screen and welcome screen, added animation using farmer-motion library
    
    * Worked on the splash screen and welcome screen, added animation using the farmer-motion library updated review requirement
    
    * updated
    
    * added typing for splashscreen usestate
    
    * Updated state vairable to camel case
    
    * Updated state vairable to camel case

---
+++ client/src/components/index.tsx
@@ -0,0 +1,11 @@
+// Common
+export { Button } from './Common/Button/Button';
+export { FingerPrint } from './Common/FingerPrint';
+
+// SplashScreen
+export { ScreenOne } from './SplashScreen/ScreenOne';
+export { ScreenTwo } from './SplashScreen/ScreenTwo';
+
+// WelcomeScreen
+export { AllowPi } from './WelcomeScreen/AllowPi';
+export { SelectLanguage } from './WelcomeScreen/SelectLanguage';
