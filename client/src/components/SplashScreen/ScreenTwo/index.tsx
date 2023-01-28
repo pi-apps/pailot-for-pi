c465f265e307d1effb2d60e2c647ab1100e626bc
Tejiri Faith <tjfaithpro@gmail.com>
2023-01-28 11:10:24 +0100

    updated

--- client/src/components/SplashScreen/ScreenTwo/index.tsx
+++ client/src/components/SplashScreen/ScreenTwo/index.tsx
@@ -1,12 +1,11 @@
+import React from 'react';
 import { logo } from '../../../assets/images';
 import styles from './ScreenTwo.module.css';
 import { motion } from 'framer-motion';
-import React from 'react';
-type Props = {
-	setNext_screen: React.Dispatch<React.SetStateAction<string>>;
-};
+import { useNavigate } from 'react-router-dom';
 
-export const ScreenTwo = ({ setNext_screen }: Props) => {
+export const ScreenTwo = () => {
+	const navigate = useNavigate();
 	return (
 		<div id={styles.wrapper}>
 			<img src={logo} />
@@ -15,7 +14,7 @@ export const ScreenTwo = ({ setNext_screen }: Props) => {
 					initial={{ width: '0%' }}
 					animate={{ width: '100%' }}
 					transition={{ duration: 10 }}
-					onAnimationComplete={() => setNext_screen('welcome')}
+					onAnimationComplete={() => navigate('/welcome')}
 					className={styles.progress_bar}
 				></motion.div>
 			</div>
--- client/src/pages/SplashScreen/SplashScreen.tsx
+++ client/src/pages/SplashScreen/SplashScreen.tsx
@@ -1,23 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { ScreenOne, ScreenTwo } from '../../components';
-import { useNavigate } from 'react-router-dom';
 
 export const SplashScreen = () => {
 	const [next_screen, setNext_screen] = useState('first_screen');
-	const navigate = useNavigate();
-	const handleNavigation = () => {
-		navigate('/welcome');
-	};
-	useEffect(() => {
-		if (next_screen == 'welcome') {
-			handleNavigation();
-		}
-	}, [next_screen]);
 
 	return (
 		<>
 			{next_screen == 'first_screen' && <ScreenOne setNext_screen={setNext_screen} />}
-			{next_screen == 'progress_bar' && <ScreenTwo setNext_screen={setNext_screen} />}
+			{next_screen == 'progress_bar' && <ScreenTwo />}
 		</>
 	);
 };
