47600194df6ed6825fc506e6ac226111d92d46e1
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2023-02-01 05:31:34 +0100

    Added image upload middlware

--- server/src/constants/environments.ts
+++ server/src/constants/environments.ts
@@ -24,6 +24,9 @@ interface Environment {
 	DATABASE_USERNAME: string;
 	DATABASE_PASSWORD: string;
 	DATABASE_PORT;
+	CLOUDINARY_NAME:string;
+    CLOUDINARY_API_KEY:string;
+	CLOUDINARY_API_SECRET:string;
 
 
 }
@@ -35,7 +38,10 @@ const env: Environment = {
 	DATABASE_TYPE: process.env.DATABASE_TYPE || 'postgres',
 	DATABASE_USERNAME: process.env.DATABASE_USERNAME || 'postgres',
 	DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || '123456',
-	DATABASE_PORT: process.env.DATABASE_PORT || 5432
+	DATABASE_PORT: process.env.DATABASE_PORT || 5432,
+	CLOUDINARY_NAME: process.env.CLOUDINARY_NAME || '',
+	CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || '',
+	CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || ''
 
 
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,22 @@ import express from 'express';
 import logger from 'morgan';
 import { corsMiddleware } from './middlewares/cors';
 import { apiRouter } from './modules/router';
+import env from "./constants/environments";
+
+
+
+
+//Cloudinary Config
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+    cloud_name: env.CLOUDINARY_NAME,
+    api_key: env.CLOUDINARY_API_KEY,
+    api_secret: env.CLOUDINARY_API_SECRET,
+    secure: true,
+});
+    
+
 
 const PORT = process.env.PORT || 3333;
 
@@ -33,6 +49,31 @@ app.get('/', (req, res) => {
 		message: 'Hello World',
 	});
 });
+
+app.post("/upload-image", async (req,res) => {
+  const image = req.body;
+
+  if (!image) {
+    return res.status(400).json({InputError: "Inputs required!"});
+    }
+
+  try {
+    const response = await cloudinary.uploader.upload(image);
+
+        if (response) {
+            console.log(response)
+            console.log("Image URL: " + response.url)
+            console.log("Image Public ID: " + response.public_id)
+
+            // Saved the URL and Public_id to DB
+
+
+        }
+  } catch (error) {
+    return res.json({error: error.message});
+}
+
+})
 
 
 app.use('/', apiRouter);
