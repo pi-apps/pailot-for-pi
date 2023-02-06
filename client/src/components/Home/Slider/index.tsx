6bc55451caff37b6a7042f9ecf0dd01f9b9095bf
tjfaithpro <tjfaithpro@gmail.com>
2023-02-06 23:47:18 +0100

    updated

--- client/src/components/Home/Slider/index.tsx
+++ client/src/components/Home/Slider/index.tsx
@@ -1,13 +1,27 @@
-import Carousel from 'react-elastic-carousel'
+import React from 'react'
+// import Carousel from 'react-elastic-carousel'
 import Styles from './Slider.module.css';
 import { Button } from '../../../components';
 import { sliderImg1 } from '../../../assets/images';
-import useSlider from './useSlider';
-export const Slider = () => {
-	const {breakPoints} = useSlider()
+import ReactElasticCarousel, { ReactElasticCarouselProps } from 'react-elastic-carousel';
+
+interface Props extends ReactElasticCarouselProps {
+	children: React.ReactNode;
+}
+
+export const Slider: React.FC<Props> = ({ children, ...props }) => {
+	// interface BreakPoints { 
+	// 	width: number;
+	// 	itemsToShow: number; itemsToScroll: number;
+	// 	}
+	// const breakPoints: BreakPoints[] = [
+	// 	{ width: 1, itemsToShow: 1, itemsToScroll:1 },
+	// 	{ width: 550, itemsToShow: 2, itemsToScroll: 1 },
+	// 	{ width: 768, itemsToShow: 3, itemsToScroll:1 },
+	// ];
 	return (
 		<div id={Styles.Wrapper}>
-			{/* <Carousel autoPlay={true} interval={3000} loop={true}>
+			<ReactElasticCarousel {...props}>
 				<div className={Styles.FirstItem}>
 					<div className={Styles.Content}>
 						<h2>Welcome to Pailot!</h2>
@@ -18,22 +32,14 @@ export const Slider = () => {
 						<Button value="Read more" />
 					</div>
 					<img src={sliderImg1} className={Styles.SliderImage} alt="" />
-				</div>
+				</div></ReactElasticCarousel>
+			{/* <Carousel showArrows={true} breakPoints={breakPoints}>
+					<div>Item 1</div>
+					<div>Item 2</div>
+					<div>Item 3</div>
+					<div>Item 4</div>
+					<div>Item 5</div>
 			</Carousel> */}
-
-			<Carousel  showArrows={true} breakPoints={breakPoints} >
-			<div className={Styles.FirstItem}>
-					<div className={Styles.Content}>
-						<h2>Welcome to Pailot!</h2>
-						<p>
-							Hub for decentralized delivery for all works of pioneers around the everyday ecosystem
-						</p>
-						<h3>FAST. DECENTRALIZED. SAFE & SECURE</h3>
-						<Button value="Read more" />
-					</div>
-					<img src={sliderImg1} className={Styles.SliderImage} alt="" />
-				</div>
-			</Carousel>
 		</div>
 	);
 };
